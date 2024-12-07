32f0f4d40f8965a3880d2813a3957fc9c017cd82
Nikita <nikistosov@gmail.com>
2024-12-07 19:07:05 +0100

    feat: baisig style header

--- portfolio/components/modules/Header/Header.tsx
+++ portfolio/components/modules/Header/Header.tsx
@@ -4,13 +4,15 @@ const Header = () => {
         <header className="header">
             <div className="container header__container">
                 <div>Nikita</div>
-                <div>
-                    <li>home</li>
-                    <li>works</li>
-                    <li>abot-me</li>
-                    <li>contacts</li>
+                <div className="header__menu">
+                    <div className="header__menu__link">
+                        <li>home</li>
+                        <li>works</li>
+                        <li>abot-me</li>
+                        <li>contacts</li>
+                    </div>
+                    <div>Lang</div>
                 </div>
-                <div>Lang</div>
             </div>
         </header>
     );
