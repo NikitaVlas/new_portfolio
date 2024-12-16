330da277bf0aa6794585a393719212cec6a4e282
Nikita <nikistosov@gmail.com>
2024-12-16 21:13:23 +0100

    fix: atr

--- portfolio/components/layouts/PageLayout.tsx
+++ portfolio/components/layouts/PageLayout.tsx
@@ -1,14 +1,10 @@
-import Header from "../modules/Header/Header.tsx";
-import Footer from "../modules/Footer/Footer.tsx";
 import MainPage from "../modules/MainPage/MainPage.tsx";
 
 const PageLayout = () => {
     return (
-        <div>
-            <Header/>
+        <body>
             <MainPage/>
-            <Footer/>
-        </div>
+        </body>
     );
 };
 
