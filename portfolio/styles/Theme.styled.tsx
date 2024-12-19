83b3b173564f37959c9ea87f2711d7bf61f56f31
Nikita <nikistosov@gmail.com>
2024-12-19 14:34:12 +0100

    feat: theme styled

---
+++ portfolio/styles/Theme.styled.tsx
@@ -0,0 +1,14 @@
+export const theme = {
+    colors: {
+        primary: "#EBEBEB",
+        secondary: "#ABB2BF",
+        backgroundColorPrimary: "#0E1117",
+        backgroundColorSecondary: "#0D1117"
+    },
+    fontsize: {
+        h1: "36px",
+        h3: "30px",
+        h4: "24px",
+        p: "16px",
+    }
+}
