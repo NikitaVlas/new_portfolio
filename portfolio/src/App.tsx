dca3c967d575719e654ff90c047ca423ae5d86b9
Nikita <nikistosov@gmail.com>
2024-12-19 14:34:39 +0100

    feat:  app container

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,15 +1,23 @@
 import Header from "../components/modules/Header/Header.tsx";
 import Footer from "../components/modules/Footer/Footer.tsx";
 import {Outlet} from "react-router-dom";
+import styled from "styled-components";
 
 const App = () => {
     return (
-        <div className="App">
+        <Container>
             <Header/>
             <Outlet/>
             <Footer/>
-        </div>
+        </Container>
     );
 };
+
+export const Container = styled.div`
+    max-width: 1024px;
+    background-color: #1D2533;
+    margin: 0 auto;
+    padding: 0 15px;
+`
 
 export default App;
