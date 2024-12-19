6046f1f6eb9e3a777bd1f536f2081994a4ea2e91
Nikita <nikistosov@gmail.com>
2024-12-19 14:33:51 +0100

    fix: styling

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -5,26 +5,29 @@ import {
     Routes,
     Route,
 } from "react-router-dom";
+import "./globalStyles/global.css"
 import App from './App.tsx'
-import "./globalStyles/globals.css"
-import "./globalStyles/header.css"
-import "./globalStyles/footer.css"
 import Works from "../public/pages/works/works.tsx";
 import PageLayout from "../components/layouts/PageLayout.tsx";
 import AboutMe from "../public/pages/about/about.tsx";
 import Contacts from "../public/pages/contacts/contacts.tsx";
+import {ThemeProvider} from "styled-components";
+import {theme} from "../styles/Theme.styled.tsx";
+
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App/>}>
-                    <Route index element={<PageLayout />} />
-                    <Route path="/pages/works" element={<Works/>}/>
-                    <Route path="/pages/about" element={<AboutMe/>}/>
-                    <Route path="/pages/contacts" element={<Contacts/>}/>
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    </StrictMode>,
+    <ThemeProvider theme={theme}>
+        <StrictMode>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App/>}>
+                        <Route index element={<PageLayout/>}/>
+                        <Route path="/pages/works" element={<Works/>}/>
+                        <Route path="/pages/about" element={<AboutMe/>}/>
+                        <Route path="/pages/contacts" element={<Contacts/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </StrictMode>
+    </ThemeProvider>
 )
