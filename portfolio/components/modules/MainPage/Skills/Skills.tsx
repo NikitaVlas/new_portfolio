b3cc3b4762887cbe60276e122672650a4399ed10
Nikita <nikistosov@gmail.com>
2025-01-26 22:05:18 +0100

    feat: translation for Skills component

--- portfolio/components/modules/MainPage/Skills/Skills.tsx
+++ portfolio/components/modules/MainPage/Skills/Skills.tsx
@@ -1,6 +1,7 @@
 import BlockTitle from "../../../elements/BlockTitle/BlockTitle";
 import Skill from "./Skill.tsx";
 import {SkillsStyled} from "../../../../src/globalStyles/skills/skills.styled.tsx";
+import {useLang} from "../../../../hooks/useLang.ts";
 
 type SkillsProps = {
     block: boolean
@@ -9,20 +10,22 @@ type SkillsProps = {
 }
 
 const Skills = ({block, span, img}: SkillsProps) => {
+    const {lang, translations} = useLang()
+
     return (
         <SkillsStyled>
-            <BlockTitle text={"skills"} span={span} img={img}/>
+            <BlockTitle text={translations[lang].skills.skills} span={span} img={img}/>
             <div className={block ? "container" : "containerAboutPage"}>
                 {block ? <div className="elements">
                     <span className={"pseudo"}/>
                     {/*<img className="doubleSquad" src="../../../../public/img/doubleSquad.svg" alt=""/>*/}
                 </div> : ""}
                 <div className="skillsContainer">
-                    <Skill block={block} title={"Languages"} skill={"JavaScript TypeScript Node.JS PHP Java"}/>
-                    <Skill block={block} title={"Databases"} skill={"MariaDB MongoDB"}/>
-                    <Skill block={block} title={"Libraries/Frameworks"} skill={"React Express.js Angular Fusion FLow SCSS MaterialUI Bootstrap"}/>
-                    <Skill block={block} title={"Tools"} skill={"WebStorm VSCode Figma Git Postman Storybook"}/>
-                    <Skill block={block} title={"Other"} skill={"HTML CSS REST NEOS CMS"}/>
+                    <Skill block={block} title={translations[lang].skills.languages} skill={"JavaScript TypeScript Node.JS PHP Java"}/>
+                    <Skill block={block} title={translations[lang].skills.databases} skill={"MariaDB MongoDB"}/>
+                    <Skill block={block} title={translations[lang].skills.frameworks} skill={"React Express.js Angular Fusion FLow SCSS MaterialUI Bootstrap"}/>
+                    <Skill block={block} title={translations[lang].skills.tools} skill={"WebStorm VSCode Figma Git Postman Storybook"}/>
+                    <Skill block={block} title={translations[lang].skills.other} skill={"HTML CSS REST NEOS CMS"}/>
                 </div>
             </div>
 
