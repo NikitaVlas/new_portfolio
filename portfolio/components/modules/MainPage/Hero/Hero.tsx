41d65c73306c43d885a5f5bbee4b133f034af5c7
Nikita <nikistosov@gmail.com>
2025-01-26 21:52:48 +0100

    feat: translation for Hero component

--- portfolio/components/modules/MainPage/Hero/Hero.tsx
+++ portfolio/components/modules/MainPage/Hero/Hero.tsx
@@ -1,21 +1,24 @@
 import LinkButton from "../../../elements/LinkButton/LinkButton.tsx";
 import {HeroStyled} from "../../../../src/globalStyles/hero/hero.styled.tsx";
+import {useLang} from "../../../../hooks/useLang.ts";
 
 const Hero = () => {
+    const {lang, translations} = useLang()
+
     return (
         <HeroStyled>
             <div className="heroBlock">
-                <h1>I'm a <span>front-end</span> developer</h1>
-                <span>I crafts responsive websites where technologies meet creativity</span>
+                <h1>{translations[lang].hero.title1}<span>{translations[lang].hero.title2}</span>{translations[lang].hero.title3}</h1>
+                <span>{translations[lang].hero.underTitle}</span>
                 <div>
-                    <LinkButton title={"Contact me!"} to={"/pages/contacts"}/>
+                    <LinkButton title={translations[lang].hero.button} to={"/pages/contacts"}/>
                 </div>
             </div>
             <div className="elements">
                 <img className="mainIMG" src="../../../../public/img/photoHero.png" alt=""/>
                 <div>
                     <img src="../../../../public/img/Rectangle.svg" alt=""/>
-                    <span>Currently working on Portfolio</span>
+                    <span>{translations[lang].hero.currently}</span>
                 </div>
             </div>
         </HeroStyled>
