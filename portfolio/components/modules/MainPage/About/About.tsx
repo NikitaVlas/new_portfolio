28726abbb55d05fe0bd132847e5120bcad742bea
Nikita <nikistosov@gmail.com>
2025-01-27 15:47:04 +0100

    feat: translation about component

--- portfolio/components/modules/MainPage/About/About.tsx
+++ portfolio/components/modules/MainPage/About/About.tsx
@@ -2,6 +2,7 @@
 import BlockTitle from "../../../elements/BlockTitle/BlockTitle";
 import LinkButton from "../../../elements/LinkButton/LinkButton.tsx";
 import {AboutStyled} from "../../../../src/globalStyles/about/about.staled.tsx";
+import {useLang} from "../../../../hooks/useLang.ts";
 
 type AboutProps = {
     button: boolean
@@ -10,20 +11,15 @@ type AboutProps = {
 }
 
 const About = ({button, span, img}: AboutProps) => {
+    const {lang, translations} = useLang()
+
     return (
         <AboutStyled>
-            <BlockTitle text={"about-me"} span={span} img={img}/>
+            <BlockTitle text={translations[lang].about.about} span={span} img={img}/>
             <div className="aboutContainer">
                 <div className="aboutDescription">
-                    <p>Hello, i’m Nikita!
-                        <br/>
-                        I’m a self-taught front-end developer based in Cologne, Germany. I can develop responsive
-                        websites from scratch and raise them into modern user-friendly web experiences.
-                        <br/>
-                        Transforming my creativity and knowledge into a websites has been my passion for over a year. I
-                        have been helping various clients to establish their presence online. I always strive to learn
-                        about the newest technologies and frameworks.</p>
-                    {button ? <LinkButton title={"Read more"} to={"/pages/about"}/> : ""}
+                    <p>{translations[lang].about.description}</p>
+                    {button ? <LinkButton title={translations[lang].about.button} to={"/pages/about"}/> : ""}
                 </div>
                 <div >
                     <img src="../../../../public/img/photoabout.png" alt=""/>
