fac2c1790a62af3399cfe0725715de1678c25270
NikitaVlas <nikistosov@gmail.com>
2025-01-30 12:53:58 +0100

    feat: test 2

--- portfolio/components/modules/Footer/Footer.tsx
+++ portfolio/components/modules/Footer/Footer.tsx
@@ -9,16 +9,26 @@ const Footer = () => {
                 <div className="footerLeft">
                     <Link className="icon" to="/">Nikita</Link>
                     <a href={"mailto:nikistosov@gmail.com"}><span>nikistosov@gmail.com</span></a>
-                    <a href={"mailto:nikistosov@gmail.com"}><span>nikistosov@gmail.com</span></a>
                 </div>
                 <div className="footerRight">
                     <div>
                         <span>Media</span>
                     </div>
                     <div className="footer-link">
-                        <a href="https://github.com/NikitaVlas" target="_blank" rel="noopener noreferrer"><img src="../../../public/img/Github.svg"
-                                                                     alt=""/></a>
-                        <a href="https://www.linkedin.com/in/nikita-vlasov-13ba3b1ab/" target="_blank" rel="noopener noreferrer"><img
+                        <a href="https://github.com/NikitaVlas" target="_blank" rel="noopener noreferrer"><img
+                            src="../../../public/img/Github.svg"
+                            alt=""/></a>
+                        <a href="https://www.linkedin.com/in/nikita-vlasov-13ba3b1ab/" target="_blank"
+                           rel="noopener noreferrer"><img
+                            src="../../../public/img/Linkedin.svg" alt=""/></a>
+                        <a href={"mailto:nikistosov@gmail.com"}><img src="../../../public/img/Email.svg" alt=""/></a>
+                        <a href={"https://t.me/sibjpeg"} target="_blank" rel="noopener noreferrer"><img
+                            src="../../../public/img/Telegram.svg" alt=""/></a>
+                        <a href="https://github.com/NikitaVlas" target="_blank" rel="noopener noreferrer"><img
+                            src="../../../public/img/Github.svg"
+                            alt=""/></a>
+                        <a href="https://www.linkedin.com/in/nikita-vlasov-13ba3b1ab/" target="_blank"
+                           rel="noopener noreferrer"><img
                             src="../../../public/img/Linkedin.svg" alt=""/></a>
                         <a href={"mailto:nikistosov@gmail.com"}><img src="../../../public/img/Email.svg" alt=""/></a>
                         <a href={"https://t.me/sibjpeg"} target="_blank" rel="noopener noreferrer"><img
