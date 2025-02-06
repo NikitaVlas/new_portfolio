c7b8a3924fc5e74376571e7db938ec1372fc8384
NikitaVlas <nikistosov@gmail.com>
2025-02-06 15:22:13 +0100

    fix: line svg by blockTItle component

--- portfolio/components/elements/BlockTitle/BlockTitle.tsx
+++ portfolio/components/elements/BlockTitle/BlockTitle.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import {theme} from "../../../styles/Theme.styled.tsx";
-
+import line from "../../../src/assets/Line.svg";
 
 type BlockTitleProps = {
     text: string
@@ -11,7 +11,7 @@ const BlockTitle = ({text, span, img}: BlockTitleProps) => {
     return (
         <BlockTitleStyle>
             <h3><span>{span}</span>{text}</h3>
-            {img ? <img src="../../../src/assets/img/Line.svg" alt=""/> : ""}
+            {img ? <img src={line} alt=""/> : ""}
         </BlockTitleStyle>
     );
 };
